35ee852332d5e61173c50c552b864152c4a64e0f
ajmenaa <165622283+ajmenaa@users.noreply.github.com>
2024-05-10 20:38:00 -0600

    Update introduccion.py

--- Flet Parte I/introduccion.py
+++ Flet Parte I/introduccion.py
@@ -1,14 +1,33 @@
 import flet
-from flet import Page, TextField
+from flet import Page, TextField, Row, IconButton, icons
 
 def main(page: Page):
     #Instrucciones para crear controles y actulizar la pagina
     page.title = 'Mi primer app en Flet'
+    page.vertical_alignment = 'center'
     
-    txt_numero = TextField()
+    txt_numero = TextField(value='0',width=100,text_align='right')
     
-    pass
-
+    def sumar_click(event):
+        txt_numero.value = int(txt_numero.value) + 1
+        page.update()
+    
+    #Dibujar UI 
+    page.add(
+        Row(
+            [
+                IconButton(icon='REMOVE',icon_color="pink600",tooltip="Restar Número"),
+                txt_numero,
+                IconButton(
+                            icons.ADD, 
+                            icon_color="green",
+                            tooltip="Sumar Número", 
+                            on_click=sumar_click
+                           )
+            ],
+            alignment='center'
+        )
+    ) 
 #Modo Desktop:
 flet.app(target=main)
 
