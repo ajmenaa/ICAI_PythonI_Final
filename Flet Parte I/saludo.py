e5fa330a0348537a118aefc42d384c7fdb458213
ajmenaa <165622283+ajmenaa@users.noreply.github.com>
2024-05-12 20:02:04 -0600

    Update saludo.py

--- Flet Parte I/saludo.py
+++ Flet Parte I/saludo.py
@@ -1,20 +1,30 @@
 import flet
-from flet import *
+from flet import Page,TextField,Text,Row,ElevatedButton
 
 def main(page: Page):
+    #Atributos Pagina
     page.title='Aplicación Saludo'
     page.vertical_alignment='center'
     
+    #Funciones
+    def saludar(e):
+        lbl_saludo.value = f'¡Hola {txt_nombre.value} esto es Flet!'
+        lbl_saludo.update()
+    
+    #Definir Controles
     txt_nombre = TextField(label='Digite su nombre')
     lbl_saludo = Text()
     
-    
-    row = Row(controls=[
+    #Dibujar UI
+    row_1 = Row(controls=[
         txt_nombre,
-        ElevatedButton(text='Saludar'),
+        ElevatedButton(text='Saludar', on_click=saludar)
+    ],alignment='center')
+    
+    row_2 = Row(controls=[
         lbl_saludo
     ],alignment='center')
 
-    page.add(row)
+    page.add(row_1, row_2)
 
 flet.app(target=main)
