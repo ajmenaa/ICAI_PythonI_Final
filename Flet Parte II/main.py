e156a011316f9c04dc5c66a6717ae68a396c90e9
ajmenaa <165622283+ajmenaa@users.noreply.github.com>
2024-05-15 19:25:41 -0600

    clase 12 avance

--- Flet Parte II/main.py
+++ Flet Parte II/main.py
@@ -6,6 +6,46 @@ from data_dolar import obtener_datos
 
 def main(page: flet.Page):
     df_data = obtener_datos()
+    print(df_data)
+
+    
+    
+    #Definir Controles
+    cbx_tipo_cambio = Dropdown(
+        options=[
+            dropdown.Option('Venta'),
+            dropdown.Option('Compra')
+        ],
+        width=300
+    )
+    
+    cbx_tipo_entidad = Dropdown( # Creo filtro por tio entidad
+        width=400
+    )
+    
+    for entidad in df_data['Tipo'].sort_values().unique():  # Cargo Filtro por tipo entidad
+        cbx_tipo_entidad.options.append(dropdown.Option(entidad))
+    
+    btn_graficar = ElevatedButton(text=('Graficar'))
+    
+    
+    #Dibujar UI 
+    page.add(
+        Row(
+            [
+             cbx_tipo_cambio,
+             cbx_tipo_entidad,
+             btn_graficar
+            ],
+            alignment='center'
+        ),
+        Row(
+            [
+             
+            ],
+            alignment='center'
+        )
+    ) 
 
 
 flet.app(target=main)
